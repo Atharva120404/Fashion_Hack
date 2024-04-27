6ab28d82e83927bd95daa1d5b644b9f8a7f7bbec
Atharva120404 <atharva.dahegaonkar23@spit.ac.in>
2024-04-27 15:44:30 +0530

    Realtive Paths Changes

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask, render_template, request, redirect, url_for, session, Response
+from flask import Flask, render_template, request, redirect, url_for, session, Response,send_from_directory
 from flask_mysqldb import MySQL
 from flask_mysqldb import MySQL
 import MySQLdb.cursors
 import re
 import cv2
 import numpy as np
+import os
 
 app = Flask(__name__)
 app.secret_key = 'test'
@@ -18,9 +19,49 @@ app.config['MYSQL_DB'] = 'user-system'
 app.config['MYSQL_PORT'] = 3306
 
 mysql = MySQL(app)
+@app.route('/static/<path:filename>')
+def serve_static(filename):
+    root_dir = os.path.dirname(os.path.abspath(__file__))
+    return send_from_directory(os.path.join(root_dir, 'static'), filename)
 
 @app.route("/")
 def home():
     return render_template("index.html")
 
-app.run(debug=True)
+@app.route("/about.html")
+def about():
+    return render_template("about.html")
+
+@app.route("/blog-details.html")
+def blog_details():
+    return render_template("blog-details.html")
+
+@app.route("/blog.html")
+def blog():
+    return render_template("blog.html")
+
+@app.route("/checkout.html")
+def checkout():
+    return render_template("checkout.html")
+
+@app.route("/contact.html")
+def contact():
+    return render_template("contact.html")
+
+@app.route("/shop-details.html")
+def shop_details():
+    return render_template("shop-details.html")
+
+@app.route("/shop.html")
+def shop():
+    return render_template("shop.html")
+
+@app.route("/shopping-cart.html")
+def shopping_cart():
+    return render_template("shop-cart.html")
+
+
+if __name__=='__main__':
+    app.run(debug=True)
+
+
